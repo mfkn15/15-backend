cb7d7923c274361bfc38599f6b64566e9677282c
mfkn15 <148982955+mfkn15@users.noreply.github.com>
2023-10-25 22:31:29 +0700

    Update whitelist.ts

--- src/middlewares/whitelist.ts
+++ src/middlewares/whitelist.ts
@@ -1,11 +1,11 @@
 
 const whitelist =  {
     clientOptionsLimited : {
-        origin: ['https://week-15-mfaisalkemal-a.netlify.app/', 'https://week-15-mfaisalkemal-b.netlify.app/', 'http://localhost:5173'],
+        origin: ['https://week-15-mfaisalkemal-a.netlify.app', 'https://week-15-mfaisalkemal-b.netlify.app', 'http://localhost:5173'],
         methods:['GET','POST']
     },
     clientOptionsGlobal : {
-        origin: 'https://week-15-mfaisalkemal-b.netlify.app/',
+        origin: 'https://week-15-mfaisalkemal-b.netlify.app',
         methods:['GET','POST','PUT','DELETE'],
     }
 }
