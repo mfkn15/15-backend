db81b1a48e751b21a803bf1ee78c02d1c084345c
mfkn15 <148982955+mfkn15@users.noreply.github.com>
2023-10-25 22:30:02 +0700

    Update whitelist.js

--- dist/middlewares/whitelist.js
+++ dist/middlewares/whitelist.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const whitelist = {
     clientOptionsLimited: {
-        origin: ['https://week-15-mfaisalkemal-a.netlify.app/', 'https://week-15-mfaisalkemal-b.netlify.app/', 'http://localhost:5173'],
+        origin: ['https://week-15-mfaisalkemal-a.netlify.app', 'https://week-15-mfaisalkemal-b.netlify.app', 'http://localhost:5173'],
         methods: ['GET', 'POST']
     },
     clientOptionsGlobal: {
-        origin: 'https://week-15-mfaisalkemal-b.netlify.app/',
+        origin: 'https://week-15-mfaisalkemal-b.netlify.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
     }
 };
