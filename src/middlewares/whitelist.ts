cd63f2b23e8957e03edaf8d6bf7f5bf12978ca13
mfkn15 <148982955+mfkn15@users.noreply.github.com>
2023-10-25 22:18:59 +0700

    Add files via upload

--- src/middlewares/whitelist.ts
+++ src/middlewares/whitelist.ts
@@ -1,11 +1,11 @@
 
 const whitelist =  {
     clientOptionsLimited : {
-        origin: ['https://a', 'https://b', 'http://localhost:5173'],
+        origin: ['https://week-15-mfaisalkemal-a.netlify.app/', 'https://week-15-mfaisalkemal-b.netlify.app/', 'http://localhost:5173'],
         methods:['GET','POST']
     },
     clientOptionsGlobal : {
-        origin: 'b',
+        origin: 'https://week-15-mfaisalkemal-b.netlify.app/',
         methods:['GET','POST','PUT','DELETE'],
     }
 }
